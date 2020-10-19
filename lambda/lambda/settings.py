1ffed43cca9342538b601efeb9b64d2cd7330cb4
Mailhot <36643298+Mailhot@users.noreply.github.com>
2020-10-19 11:24:14 -0400

    Update settings.py
    removed extra parentheisis

--- lambda/lambda/settings.py
+++ lambda/lambda/settings.py
@@ -16,7 +16,7 @@ from . import local_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.abspath(BASE_DIR, '..', 'base/static/base'))
+STATIC_ROOT = os.path.abspath(BASE_DIR, '..', 'base/static/base')
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
