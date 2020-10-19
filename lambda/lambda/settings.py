705332717034274e3bdb9242c5b3de148162a187
Mailhot <36643298+Mailhot@users.noreply.github.com>
2020-10-19 11:22:02 -0400

    Update settings.py
    Added static dir location

--- lambda/lambda/settings.py
+++ lambda/lambda/settings.py
@@ -16,6 +16,12 @@ from . import local_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.abspath(BASE_DIR, '..', 'base/static/base'))
+
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
