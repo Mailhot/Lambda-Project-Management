832baf0e78982c5b56e13ad3455e4dab8b432260
Mailhot <36643298+Mailhot@users.noreply.github.com>
2020-12-14 16:28:12 -0500

    playing with static link

--- lambda/lambda/settings.py
+++ lambda/lambda/settings.py
@@ -16,7 +16,7 @@ from . import local_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'base/static/base')
+STATIC_ROOT = os.path.join(BASE_DIR, 'base/static')
 print('static root', STATIC_ROOT)
 
 STATIC_URL = '/static/'
