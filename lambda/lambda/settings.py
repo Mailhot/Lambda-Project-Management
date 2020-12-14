4814a60a6f6fe323e3eebc01fddfedd1e38a855d
Mailhot <36643298+Mailhot@users.noreply.github.com>
2020-12-14 17:25:51 -0500

    fixing static dir

--- lambda/lambda/settings.py
+++ lambda/lambda/settings.py
@@ -16,7 +16,7 @@ from . import local_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'base/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 print('static root', STATIC_ROOT)
 
 STATIC_URL = '/static/'
